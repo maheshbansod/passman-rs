01fb5a6ea4c29e10224c4d73b4c196d532b2f6eb
Mahesh Bansod <mahesh0bansod@gmail.com>
2021-08-21 20:25:30 +0530

    added cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+//! # password manager that syncs with Google Drive
+//! So what I want to do is create a command line application which I can use as follows:
+//! ```
+//! $ passman save for github.com user maheshbansod pass thegithubpassword
+//! $ passman genpass 12
+//! $ passman save for <website> user <username> (pass <password>|genpass [<len>])
+//! $ passman get for <website> user <username>
+//! $ passman toclip for <website> user <username>
+//! $ passman sync
+//!
+//!
+
+use structopt::StructOpt;
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+
+    match args {
+        Cli::Save {
+            for_what,
+            user,
+            pass,
+        } => {
+            println!("{} {} {}", for_what, user, pass);
+        }
+        Cli::GenPass {} => {}
+        Cli::Get {} => {}
+        Cli::ToClip {} => {}
+        Cli::Sync {} => {}
+    }
+    // println!("{:?}", args);
+}
+
+#[derive(Debug, StructOpt)]
+enum Cli {
+    Save {
+        #[structopt(short = "f", long = "for")]
+        for_what: String,
+        #[structopt(short, long)]
+        user: String,
+        #[structopt(short, long)]
+        pass: String,
+    },
+    GenPass {},
+    Get {},
+    ToClip {},
+    Sync {},
 }
